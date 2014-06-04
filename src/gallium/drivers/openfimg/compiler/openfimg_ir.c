78b8ce1be8da187c54a4432058b54b75aeda8d62
Tomasz Figa <tomasz.figa@gmail.com>
2014-06-04 22:26:58 +0200

    openfimg: Compiler refactoring
    Complete redesign of IR structures and APIs to prepare grounds for
    SSA'ization and optimization/validation passes.
    
    Signed-off-by: Tomasz Figa <tomasz.figa@gmail.com>

--- src/gallium/drivers/openfimg/compiler/openfimg_ir.c
+++ src/gallium/drivers/openfimg/compiler/openfimg_ir.c
@@ -1,9 +1,5 @@
 /*
- * Copyright (C) 2013 Tomasz Figa <tomasz.figa@gmail.com>
- *
- * Parts shamelessly copied from Freedreno driver:
- *
- * Copyright (C) 2012 Rob Clark <robclark@freedesktop.org>
+ * Copyright (C) 2013-2014 Tomasz Figa <tomasz.figa@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -40,72 +36,553 @@
 #define WARN_MSG(f, ...)   DBG("WARN:  "f, ##__VA_ARGS__)
 #define ERROR_MSG(f, ...)  DBG("ERROR: "f, ##__VA_ARGS__)
 
-#define REG_MASK 0x3f
+const struct of_ir_opc_info of_ir_opc_info[] = {
+	[OF_OP_NOP] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 0,
+	},
+	[OF_OP_MOV] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 1,
+	},
+	[OF_OP_MOVA] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 1,
+	},
+	[OF_OP_MOVC] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 2,
+	},
+	[OF_OP_ADD] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 2,
+	},
+	[OF_OP_MUL] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 2,
+	},
+	[OF_OP_MUL_LIT] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 2,
+	},
+	[OF_OP_DP3] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 2,
+	},
+	[OF_OP_DP4] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 2,
+	},
+	[OF_OP_DPH] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 2,
+	},
+	[OF_OP_DST] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 2,
+	},
+	[OF_OP_EXP] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 1,
+	},
+	[OF_OP_EXP_LIT] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 1,
+	},
+	[OF_OP_LOG] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 1,
+	},
+	[OF_OP_LOG_LIT] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 1,
+	},
+	[OF_OP_RCP] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 1,
+	},
+	[OF_OP_RSQ] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 1,
+	},
+	[OF_OP_DP2ADD] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 3,
+	},
+	[OF_OP_MAX] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 2,
+	},
+	[OF_OP_MIN] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 2,
+	},
+	[OF_OP_SGE] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 2,
+	},
+	[OF_OP_SLT] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 2,
+	},
+	[OF_OP_SETP_EQ] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 2,
+	},
+	[OF_OP_SETP_GE] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 2,
+	},
+	[OF_OP_SETP_GT] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 2,
+	},
+	[OF_OP_SETP_NE] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 2,
+	},
+	[OF_OP_CMP] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 3,
+	},
+	[OF_OP_MAD] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 3,
+	},
+	[OF_OP_FRC] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 1,
+	},
+	[OF_OP_FLR] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 1,
+	},
+	[OF_OP_TEXLD] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 2,
+	},
+	[OF_OP_CUBEDIR] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 1,
+	},
+	[OF_OP_MAXCOMP] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 1,
+	},
+	[OF_OP_TEXLDC] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 3,
+	},
+	[OF_OP_TEXKILL] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 1,
+	},
+	[OF_OP_MOVIPS] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 1,
+	},
+	[OF_OP_ADDI] = {
+		.type = OF_IR_ALU,
+		.num_srcs = 2,
+	},
+	[OF_OP_B] = {
+		.type = OF_IR_CF,
+		.num_srcs = 0,
+	},
+	[OF_OP_BF] = {
+		.type = OF_IR_CF,
+		.num_srcs = 1,
+	},
+	[OF_OP_BP] = {
+		.type = OF_IR_CF,
+		.num_srcs = 0,
+	},
+	[OF_OP_BFP] = {
+		.type = OF_IR_CF,
+		.num_srcs = 1,
+	},
+	[OF_OP_BZP] = {
+		.type = OF_IR_CF,
+		.num_srcs = 1,
+	},
+	[OF_OP_CALL] = {
+		.type = OF_IR_CF,
+		.num_srcs = 0,
+	},
+	[OF_OP_CALLNZ] = {
+		.type = OF_IR_CF,
+		.num_srcs = 1,
+	},
+	[OF_OP_RET] = {
+		.type = OF_IR_CF,
+		.num_srcs = 0,
+	},
+};
 
-/* simple allocator to carve allocations out of an up-front allocated heap,
+struct of_ir_reg_info {
+	enum of_instr_src src_type;
+	enum of_instr_dst dst_type;
+
+	unsigned num_reads;
+	unsigned num_regs;
+
+	bool writable :1;
+	bool readable :1;
+	bool scalar :1;
+	bool al_addr :1;
+	bool a0_addr :1;
+};
+
+static const struct of_ir_reg_info vs_reg_info[OF_IR_NUM_REG_TYPES] = {
+	[OF_IR_REG_R] = {
+		.src_type = OF_SRC_R,
+		.dst_type = OF_DST_R,
+		.num_reads = 2,
+		.num_regs = 32,
+		.writable = true,
+		.readable = true,
+		.al_addr = true,
+	},
+	[OF_IR_REG_V] = {
+		.src_type = OF_SRC_V,
+		.num_reads = 1,
+		.num_regs = 10,
+		.readable = true,
+		.al_addr = true,
+	},
+	[OF_IR_REG_C] = {
+		.src_type = OF_SRC_C,
+		.num_reads = 1,
+		.num_regs = 256,
+		.readable = true,
+		.a0_addr = true,
+		.al_addr = true,
+	},
+	[OF_IR_REG_I] = {
+		.src_type = OF_SRC_I,
+		.num_reads = 1,
+		.num_regs = 16,
+		.readable = true,
+	},
+	[OF_IR_REG_AL] = {
+		.src_type = OF_SRC_AL,
+		.dst_type = OF_DST_AL,
+		.num_reads = 1,
+		.num_regs = 4,
+		.writable = true,
+		.readable = true,
+	},
+	[OF_IR_REG_B] = {
+		.src_type = OF_SRC_B,
+		.num_reads = 1,
+		.num_regs = 16,
+		.readable = true,
+		.scalar = true,
+	},
+	[OF_IR_REG_P] = {
+		.src_type = OF_SRC_P,
+		.dst_type = OF_DST_P,
+		.num_reads = 1,
+		.num_regs = 7,
+		.writable = true,
+		.readable = true,
+	},
+	[OF_IR_REG_S] = {
+		.src_type = OF_SRC_S,
+		.num_reads = 1,
+		.num_regs = 4,
+		.readable = true,
+	},
+	[OF_IR_REG_O] = {
+		.dst_type = OF_DST_O,
+		.num_regs = 10,
+		.writable = true,
+		.al_addr = true,
+	},
+	[OF_IR_REG_A0] = {
+		.dst_type = OF_DST_A0,
+		.num_regs = 1,
+		.writable = true,
+	},
+};
+
+static const struct of_ir_reg_info ps_reg_info[OF_IR_NUM_REG_TYPES] = {
+	[OF_IR_REG_R] = {
+		.src_type = OF_SRC_R,
+		.dst_type = OF_DST_R,
+		.num_reads = 2,
+		.num_regs = 32,
+		.writable = true,
+		.readable = true,
+		.al_addr = true,
+	},
+	[OF_IR_REG_V] = {
+		.src_type = OF_SRC_V,
+		.num_reads = 1,
+		.num_regs = 8,
+		.readable = true,
+		.al_addr = true,
+	},
+	[OF_IR_REG_C] = {
+		.src_type = OF_SRC_C,
+		.num_reads = 1,
+		.num_regs = 256,
+		.readable = true,
+		.a0_addr = true,
+		.al_addr = true,
+	},
+	[OF_IR_REG_I] = {
+		.src_type = OF_SRC_I,
+		.num_reads = 1,
+		.num_regs = 16,
+		.readable = true,
+	},
+	[OF_IR_REG_AL] = {
+		.src_type = OF_SRC_AL,
+		.dst_type = OF_DST_AL,
+		.num_reads = 1,
+		.num_regs = 4,
+		.writable = true,
+		.readable = true,
+	},
+	[OF_IR_REG_B] = {
+		.src_type = OF_SRC_B,
+		.num_reads = 1,
+		.num_regs = 16,
+		.readable = true,
+		.scalar = true,
+	},
+	[OF_IR_REG_P] = {
+		.src_type = OF_SRC_P,
+		.dst_type = OF_DST_P,
+		.num_reads = 1,
+		.num_regs = 7,
+		.writable = true,
+		.readable = true,
+	},
+	[OF_IR_REG_S] = {
+		.src_type = OF_SRC_S,
+		.num_reads = 1,
+		.num_regs = 8,
+		.readable = true,
+	},
+	[OF_IR_REG_D] = {
+		.src_type = OF_SRC_D,
+		.num_reads = 1,
+		.num_regs = 8,
+		.readable = true,
+	},
+	[OF_IR_REG_VFACE] = {
+		.src_type = OF_SRC_VFACE,
+		.num_reads = 1,
+		.num_regs = 1,
+		.readable = true,
+		.scalar = true,
+	},
+	[OF_IR_REG_VPOS] = {
+		.src_type = OF_SRC_VPOS,
+		.num_reads = 1,
+		.num_regs = 1,
+		.readable = true,
+		.scalar = true,
+	},
+	[OF_IR_REG_O] = {
+		.dst_type = OF_DST_O,
+		.num_regs = 1,
+		.writable = true,
+		.al_addr = true,
+	},
+	[OF_IR_REG_A0] = {
+		.dst_type = OF_DST_A0,
+		.num_regs = 1,
+		.writable = true,
+	},
+};
+
+/*
+ * Utility functions.
+ */
+
+/*
+ * Simple allocator to carve allocations out of an up-front allocated heap,
  * so that we can free everything easily in one shot.
  */
 static void *
 of_ir_alloc(struct of_ir_shader *shader, int sz)
 {
 	void *ptr = &shader->heap[shader->heap_idx];
-	shader->heap_idx += align(sz, 4);
+
+	shader->heap_idx += align(sz, 4) / 4;
+
 	return ptr;
 }
 
-static char *
-of_ir_strdup(struct of_ir_shader *shader, const char *str)
+/*
+ * Register-level operations.
+ */
+
+struct of_ir_register *
+of_ir_reg_create(struct of_ir_shader *shader, enum of_ir_reg_type type,
+		 unsigned num, const char *swizzle, unsigned flags)
 {
-	char *ptr = NULL;
-	if (str) {
-		int len = strlen(str);
-		ptr = of_ir_alloc(shader, len+1);
-		memcpy(ptr, str, len);
-		ptr[len] = '\0';
-	}
-	return ptr;
+	struct of_ir_register *reg = of_ir_alloc(shader, sizeof(*reg));
+
+	DEBUG_MSG("%x, %d, %s", flags, num, swizzle);
+	reg->flags = flags;
+	reg->type = type;
+	reg->num = num;
+	memcpy(reg->swizzle, swizzle, sizeof(reg->swizzle));
+
+	return reg;
 }
+
+struct of_ir_register *
+of_ir_reg_clone(struct of_ir_shader *shader, struct of_ir_register *src)
+{
+	struct of_ir_register *reg = of_ir_alloc(shader, sizeof(*reg));
+
+	memcpy(reg, src, sizeof(*reg));
+
+	return reg;
+}
+
+struct of_ir_register *
+of_ir_reg_temporary(struct of_ir_shader *shader)
+{
+	return of_ir_reg_create(shader, OF_IR_REG_R,
+				32 + shader->num_temporaries++, "xyzw", 0);
+}
+
+struct of_ir_register *
+of_ir_reg_immediate(struct of_ir_shader *shader)
+{
+	return of_ir_reg_create(shader, OF_IR_REG_C,
+				shader->num_immediates++, "xyzw", 0);
+}
+
+void
+of_ir_reg_set_swizzle(struct of_ir_register *reg, const char *swizzle)
+{
+	memcpy(reg->swizzle, swizzle, sizeof(reg->swizzle));
+}
+
+/*
+ * instruction-level operations.
+ */
+
+struct of_ir_instruction *
+of_ir_instr_create(struct of_ir_shader *shader, enum of_instr_opcode opc)
+{
+	struct of_ir_instruction *instr = of_ir_alloc(shader, sizeof(*instr));
+
+	DEBUG_MSG("%d", opc);
+	instr->opc = opc;
+
+	return instr;
+}
+
+void
+of_ir_instr_add_dst(struct of_ir_instruction *instr, struct of_ir_register *reg)
+{
+	assert(!instr->dst);
+	instr->dst = reg;
+}
+
+void
+of_ir_instr_add_src(struct of_ir_instruction *instr, struct of_ir_register *reg)
+{
+	assert(instr->num_srcs < ARRAY_SIZE(instr->srcs));
+	instr->srcs[instr->num_srcs++] = reg;
+}
+
+void
+of_ir_instr_insert(struct of_ir_shader *shader, struct of_ir_cf_block *block,
+		   struct of_ir_instruction *where,
+		   struct of_ir_instruction *instr)
+{
+	const struct of_ir_opc_info *info = of_ir_get_opc_info(instr->opc);
+
+	assert(instr->num_srcs == info->num_srcs);
+	assert(info->type != OF_IR_CF);
+
+	if (where) {
+		list_addtail(&instr->list, &where->list);
+		return;
+	}
+
+	if (!block)
+		block = LIST_ENTRY(struct of_ir_cf_block,
+				shader->cf_blocks.prev, list);
+
+	list_addtail(&instr->list, &block->instrs);
+}
+
+/*
+ * Basic block-level operations.
+ */
+
+struct of_ir_cf_block *
+of_ir_cf_create(struct of_ir_shader *shader)
+{
+	struct of_ir_cf_block *cf = of_ir_alloc(shader, sizeof(*cf));
+	int i;
+
+	LIST_INITHEAD(&cf->psis);
+	LIST_INITHEAD(&cf->instrs);
+
+	for (i = 0; i < OF_IR_NUM_CF_TARGETS; ++i)
+		LIST_INITHEAD(&cf->targets[i].list);
+
+	return cf;
+}
+
+void
+of_ir_cf_insert(struct of_ir_shader *shader, struct of_ir_cf_block *where,
+		struct of_ir_cf_block *block)
+{
+	struct list_head *head;
+
+	if (where)
+		head = &where->list;
+	else
+		head = &shader->cf_blocks;
+
+	list_addtail(&block->list, head);
+}
+
+/*
+ * Shader-level operations.
+ */
 
 struct of_ir_shader *
 of_ir_shader_create(void)
 {
+	struct of_ir_shader *shader;
+	struct of_ir_cf_block *cf;
+
 	DEBUG_MSG("");
-	return calloc(1, sizeof(struct of_ir_shader));
+	shader = CALLOC_STRUCT(of_ir_shader);
+	if (!shader)
+		return NULL;
+
+	LIST_INITHEAD(&shader->cf_blocks);
+
+	cf = of_ir_cf_create(shader);
+	of_ir_cf_insert(shader, NULL, cf);
+
+	return shader;
 }
 
 void
 of_ir_shader_destroy(struct of_ir_shader *shader)
 {
 	DEBUG_MSG("");
-	free(shader);
+	FREE(shader);
 }
 
-static struct of_ir_instruction *
-of_ir_instr_create(struct of_ir_shader *shader, enum of_instr_opcode opc,
-		   enum of_ir_instr_type type)
-{
-	struct of_ir_instruction *instr =
-			of_ir_alloc(shader, sizeof(struct of_ir_instruction));
-	DEBUG_MSG("%d", opc);
-	instr->opc = opc;
-	instr->instr_type = type;
-	instr->next_3arg = false;
-	assert(shader->instrs_count < ARRAY_SIZE(shader->instrs));
-	shader->instrs[shader->instrs_count++] = instr;
-	return instr;
-}
-
-struct of_ir_instruction *
-of_ir_instr_create_alu(struct of_ir_shader *shader, enum of_instr_opcode opc)
-{
-	return of_ir_instr_create(shader, opc, OF_IR_ALU);
-}
-
-struct of_ir_instruction *
-of_ir_instr_create_cf(struct of_ir_shader *shader, enum of_instr_opcode opc)
-{
-	return of_ir_instr_create(shader, opc, OF_IR_CF);
-}
+/*
+ * Assembler.
+ */
 
 static void
 reg_update_stats(struct of_ir_register *reg, struct of_ir_shader_info *info,
@@ -135,7 +612,6 @@ reg_update_stats(struct of_ir_register *reg, struct of_ir_shader_info *info,
 	}
 }
 
-/* actually, a write-mask */
 static uint32_t
 dst_mask(struct of_ir_register *reg)
 {
@@ -187,39 +663,31 @@ src_swiz(struct of_ir_register *reg)
 	return swiz;
 }
 
-/*
- * Control flow instructions:
- */
-
 static void
-instr_emit_cf(struct of_ir_instruction *instr, uint32_t *dwords,
+instr_emit_cf(struct of_ir_cf_block *cf, uint32_t *dwords,
 	      struct of_ir_shader_info *info)
 {
 	DBG("TODO");
 }
 
-/*
- * ALU instructions:
- */
-
 static void
 instr_emit_alu(struct of_ir_instruction *instr, uint32_t *dwords,
 	       struct of_ir_shader_info *info)
 {
-	int reg = 0;
-	struct of_ir_register *dst_reg = instr->dst;
 	struct of_ir_register *src0_reg;
 	struct of_ir_register *src1_reg;
 	struct of_ir_register *src2_reg;
+	struct of_ir_register *dst_reg;
 
-	src0_reg = instr->src[reg++];
-	src1_reg = instr->src[reg++];
-	src2_reg = instr->src[reg++];
+	memset(dwords, 0, 4 * sizeof(uint32_t));
 
-	reg_update_stats(dst_reg, info, true);
-	reg_update_stats(src0_reg, info, false);
+	dwords[2] |= INSTR_WORD2_OPCODE(instr->opc);
+
+	if (instr->flags & OF_IR_INSTR_NEXT_3SRC)
+		dwords[2] |= INSTR_WORD2_NEXT_3SRC;
 
 	/* Source register 1 */
+	src2_reg = instr->srcs[2];
 	if (src2_reg) {
 		reg_update_stats(src2_reg, info, false);
 
@@ -237,6 +705,7 @@ instr_emit_alu(struct of_ir_instruction *instr, uint32_t *dwords,
 	}
 
 	/* Source register 1 */
+	src1_reg = instr->srcs[1];
 	if (src1_reg) {
 		reg_update_stats(src1_reg, info, false);
 
@@ -254,6 +723,10 @@ instr_emit_alu(struct of_ir_instruction *instr, uint32_t *dwords,
 	}
 
 	/* Source register 0 (always used) */
+	src0_reg = instr->srcs[0];
+	assert(src0_reg);
+	reg_update_stats(src0_reg, info, false);
+
 	assert(!src0_reg->swizzle || (strlen(src0_reg->swizzle) == 4));
 
 	dwords[1] |= INSTR_WORD1_SRC0_NUM(src0_reg->num) |
@@ -268,6 +741,10 @@ instr_emit_alu(struct of_ir_instruction *instr, uint32_t *dwords,
 	dwords[2] |= INSTR_WORD2_SRC0_SWIZZLE(src_swiz(src0_reg));
 
 	/* Destination register */
+	dst_reg = instr->dst;
+	assert(dst_reg);
+	reg_update_stats(dst_reg, info, true);
+
 	assert(!dst_reg->swizzle || (strlen(dst_reg->swizzle) == 4));
 
 	dwords[2] |= ALU_WORD2_DST_NUM(dst_reg->num) |
@@ -277,40 +754,18 @@ instr_emit_alu(struct of_ir_instruction *instr, uint32_t *dwords,
 	if (dst_reg->flags & OF_IR_REG_SAT)
 		dwords[2] |= ALU_WORD2_DST_SAT;
 
-	/* TODO: Implement prediate support */
-}
-
-static int
-instr_emit(struct of_ir_instruction *instr, uint32_t *dwords, uint32_t idx,
-	   struct of_ir_shader_info *info)
-{
-	memset(dwords, 0, 4 * sizeof(uint32_t));
-
-	dwords[2] |= INSTR_WORD2_OPCODE(instr->opc);
-
-	if (instr->next_3arg)
-		dwords[2] |= INSTR_WORD2_NEXT_3SRC;
-
-	switch (instr->instr_type) {
-	case OF_IR_CF:
-		instr_emit_cf(instr, dwords, info);
-	case OF_IR_ALU:
-		instr_emit_alu(instr, dwords, info);
-	}
-
-	return 0;
+	/* TODO: Implement predicate support */
 }
 
 struct pipe_resource *
 of_ir_shader_assemble(struct of_context *ctx, struct of_ir_shader *shader,
 		      struct of_ir_shader_info *info)
 {
-	uint32_t i;
 	uint32_t *ptr, *dwords = NULL;
-	uint32_t idx = 0;
-	int ret;
 	struct pipe_resource *buffer;
 	struct pipe_transfer *transfer;
+	struct of_ir_cf_block *cf;
+	unsigned idx = 0;
 
 	assert(shader->instrs_count);
 
@@ -334,16 +789,39 @@ of_ir_shader_assemble(struct of_context *ctx, struct of_ir_shader *shader,
 		goto fail;
 	}
 
-	/* third pass, emit ALU/FETCH: */
-	for (i = 0; i < shader->instrs_count; i++) {
-		ret = instr_emit(shader->instrs[i], ptr, idx++, info);
-		if (ret) {
-			ERROR_MSG("instruction emit failed: %d", ret);
-			goto fail;
+	/*
+	 * First pass:
+	 * Emit ALU instructions and assign addresses to CF blocks.
+	 */
+	LIST_FOR_EACH_ENTRY(cf, &shader->cf_blocks, list) {
+		struct of_ir_instruction *ins;
+
+		/* Remember start address of the block. */
+		cf->address = idx;
+
+		/* Emit each instruction of the basic block. */
+		LIST_FOR_EACH_ENTRY(ins, &cf->instrs, list) {
+			instr_emit_alu(ins, ptr, info);
+			ptr += 4;
+			++idx;
 		}
-		ptr += 4;
-		assert((ptr - dwords) <= info->sizedwords);
+
+		if (cf->cf_instr) {
+			/*
+			 * Keep place for CF instruction, that will be emitted
+			 * in second pass.
+			 */
+			ptr += 4;
+			++idx;
+		}
 	}
+
+	/*
+	 * Second pass:
+	 * Emit CF instructions, as we know addresses of all basic blocks now.
+	 */
+	LIST_FOR_EACH_ENTRY(cf, &shader->cf_blocks, list)
+		instr_emit_cf(cf, dwords, info);
 
 	if (transfer)
 		pipe_buffer_unmap(&ctx->base, transfer);
@@ -356,29 +834,4 @@ fail:
 	if (buffer)
 		pipe_resource_reference(&buffer, NULL);
 	return NULL;
-}
-
-struct of_ir_register *
-of_ir_reg_create(struct of_ir_shader *shader, struct of_ir_instruction *instr,
-		 int num, const char *swizzle, int flags, int type)
-{
-	struct of_ir_register *reg = of_ir_alloc(shader, sizeof(*reg));
-
-	DEBUG_MSG("%x, %d, %s", flags, num, swizzle);
-	assert(num <= REG_MASK);
-	reg->flags = flags;
-	reg->num = num;
-	reg->swizzle = of_ir_strdup(shader, swizzle);
-	reg->type = type;
-	assert(instr->src_count < ARRAY_SIZE(instr->src));
-	instr->src[instr->src_count++] = reg;
-
-	if (instr->src_count == 4) {
-		assert(shader->instrs_count > 1);
-
-		instr = shader->instrs[shader->instrs_count - 2];
-		instr->next_3arg = true;
-	}
-
-	return reg;
 }
