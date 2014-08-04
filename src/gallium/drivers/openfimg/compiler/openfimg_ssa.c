c44e0b0d596cc30380b3b2116d268ef7658a9fca
Tomasz Figa <tomasz.figa@gmail.com>
2014-08-04 21:23:33 +0200

    openfimg: compiler: Find out variables defined for AST nodes
    Signed-off-by: Tomasz Figa <tomasz.figa@gmail.com>

--- src/gallium/drivers/openfimg/compiler/openfimg_ssa.c
+++ src/gallium/drivers/openfimg/compiler/openfimg_ssa.c
@@ -31,10 +31,99 @@
 #include "openfimg_ir_priv.h"
 #include "openfimg_util.h"
 
+struct of_ir_ssa {
+	struct of_ir_shader *shader;
+	unsigned vars_bitmap_size;
+	unsigned vars_bitmap_bits;
+	struct of_heap *heap;
+};
+
+static void
+variables_defined_list(struct of_ir_ssa *ssa, struct of_ir_ast_node *node)
+{
+	struct of_ir_instruction *ins;
+
+	LIST_FOR_EACH_ENTRY(ins, &node->list.instrs, list) {
+		struct of_ir_register *dst = ins->dst;
+
+		if (!dst || dst->type != OF_IR_REG_R)
+			continue;
+
+		of_bitmap_set(node->ssa.vars_defined, dst->num);
+	}
+}
+
+static void
+variables_defined(struct of_ir_ssa *ssa, struct of_ir_ast_node *node)
+{
+	struct of_ir_ast_node *child, *parent;
+	uint32_t *vars_defined;
+
+	vars_defined = of_heap_alloc(ssa->heap, ssa->vars_bitmap_size);
+	node->ssa.vars_defined = vars_defined;
+
+	LIST_FOR_EACH_ENTRY(child, &node->nodes, parent_list)
+		variables_defined(ssa, child);
+
+	if (node->type == OF_IR_NODE_DEPART || node->type == OF_IR_NODE_REPEAT)
+		return;
+
+	if (node->type == OF_IR_NODE_LIST)
+		variables_defined_list(ssa, node);
+
+	parent = node->parent;
+	if (!parent)
+		return;
+
+	if (parent->type == OF_IR_NODE_DEPART
+	    || parent->type == OF_IR_NODE_REPEAT)
+		parent = parent->depart_repeat.region;
+
+	of_bitmap_or(parent->ssa.vars_defined, parent->ssa.vars_defined,
+			vars_defined, ssa->vars_bitmap_bits);
+}
+
+
+
+static void
+dump_ssa_data(struct of_ir_shader *shader, struct of_ir_ast_node *node,
+	      unsigned level, void *data)
+{
+	struct of_ir_ssa *ssa = data;
+	unsigned bit;
+
+	if (node->type == OF_IR_NODE_LIST)
+		return;
+
+	_debug_printf("%*s# vars_defined:", level, "");
+	OF_BITMAP_FOR_EACH_SET_BIT(bit, node->ssa.vars_defined,
+				   ssa->vars_bitmap_bits) {
+		_debug_printf(" R%d", bit);
+	}
+	_debug_printf("\n");
+}
+
 int
 of_ir_to_ssa(struct of_ir_shader *shader)
 {
-	/* TODO: Convert IR to SSA. */
+	struct of_ir_ast_node *node;
+	struct of_ir_ssa *ssa;
+	struct of_heap *heap;
+
+	heap = of_heap_create();
+	ssa = of_heap_alloc(heap, sizeof(*ssa));
+	ssa->heap = heap;
+	ssa->shader = shader;
+	ssa->vars_bitmap_bits = shader->stats.num_vars;
+	ssa->vars_bitmap_size = OF_BITMAP_WORDS_FOR_BITS(ssa->vars_bitmap_bits)
+				* sizeof(uint32_t);
+
+	LIST_FOR_EACH_ENTRY(node, &shader->root_nodes, parent_list)
+		variables_defined(ssa, node);
+
+	of_ir_dump_ast(shader, dump_ssa_data, ssa);
+
+	of_heap_destroy(heap);
 
 	return 0;
 }
