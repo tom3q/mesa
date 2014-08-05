29dc7d9f28836eeda0587fb407e8dafd941d6e2c
Tomasz Figa <tomasz.figa@gmail.com>
2014-08-06 01:50:45 +0200

    openfimg: compiler: Implement variable renaming
    This gives us a program in SSA form, although I'm not yet sure if it's
    fully correct...
    
    Signed-off-by: Tomasz Figa <tomasz.figa@gmail.com>

--- src/gallium/drivers/openfimg/compiler/openfimg_ssa.c
+++ src/gallium/drivers/openfimg/compiler/openfimg_ssa.c
@@ -36,6 +36,9 @@ struct of_ir_ssa {
 	unsigned vars_bitmap_size;
 	unsigned num_vars;
 	struct of_heap *heap;
+	struct of_stack *renames_stack;
+	unsigned *renames;
+	unsigned *def_count;
 };
 
 struct of_ir_phi {
@@ -162,6 +165,114 @@ insert_phi(struct of_ir_ssa *ssa, struct of_ir_ast_node *node)
 }
 
 static void
+rename_phi_operand(struct of_ir_ssa *ssa, unsigned num, struct of_ir_phi *phi,
+		   unsigned *renames)
+{
+	phi->src[num] = renames[phi->reg];
+}
+
+static void
+rename_lhs(struct of_ir_ssa *ssa, struct of_ir_phi *phi, unsigned *renames)
+{
+	phi->dst = ++ssa->def_count[phi->reg];
+	ssa->renames[phi->reg] = phi->dst;
+}
+
+static void
+make_ssa(struct of_ir_ssa *ssa, struct of_ir_ast_node *node)
+{
+	struct of_ir_instruction *ins;
+	struct of_ir_ast_node *region;
+	struct of_ir_ast_node *child;
+	unsigned *old_renames;
+	struct of_ir_phi *phi;
+	unsigned i;
+
+	switch (node->type) {
+	case OF_IR_NODE_REGION:
+		LIST_FOR_EACH_ENTRY(phi, &node->ssa.loop_phis, list) {
+			rename_phi_operand(ssa, 0, phi, ssa->renames);
+			rename_lhs(ssa, phi, ssa->renames);
+		}
+
+		LIST_FOR_EACH_ENTRY(child, &node->nodes, parent_list)
+			make_ssa(ssa, child);
+
+		LIST_FOR_EACH_ENTRY(phi, &node->ssa.phis, list)
+			rename_lhs(ssa, phi, ssa->renames);
+		break;
+
+	case OF_IR_NODE_IF_THEN:
+		LIST_FOR_EACH_ENTRY(phi, &node->ssa.phis, list)
+			rename_phi_operand(ssa, 0, phi, ssa->renames);
+
+		LIST_FOR_EACH_ENTRY(child, &node->nodes, parent_list)
+			make_ssa(ssa, child);
+
+		LIST_FOR_EACH_ENTRY(phi, &node->ssa.phis, list) {
+			rename_phi_operand(ssa, 1, phi, ssa->renames);
+			rename_lhs(ssa, phi, ssa->renames);
+		}
+		break;
+
+	case OF_IR_NODE_DEPART:
+		old_renames = ssa->renames;
+		ssa->renames = of_stack_push(ssa->renames_stack);
+		memset(ssa->renames, 0, ssa->num_vars * sizeof(*ssa->renames));
+
+		region = node->depart_repeat.region;
+		LIST_FOR_EACH_ENTRY(phi, &region->ssa.phis, list)
+			ssa->renames[phi->reg] = old_renames[phi->reg];
+
+		LIST_FOR_EACH_ENTRY(child, &node->nodes, parent_list)
+			make_ssa(ssa, child);
+
+		LIST_FOR_EACH_ENTRY(phi, &region->ssa.phis, list)
+			rename_phi_operand(ssa, node->ssa.depart_number, phi,
+						ssa->renames);
+
+		ssa->renames = of_stack_pop(ssa->renames_stack);
+		break;
+
+	case OF_IR_NODE_REPEAT:
+		LIST_FOR_EACH_ENTRY(child, &node->nodes, parent_list)
+			make_ssa(ssa, child);
+
+		region = node->depart_repeat.region;
+		LIST_FOR_EACH_ENTRY(phi, &region->ssa.loop_phis, list)
+			rename_phi_operand(ssa, node->ssa.repeat_number, phi,
+						ssa->renames);
+		break;
+
+	case OF_IR_NODE_LIST:
+		LIST_FOR_EACH_ENTRY(ins, &node->list.instrs, list) {
+			struct of_ir_register *dst = ins->dst;
+
+			for (i = 0; i < OF_IR_NUM_SRCS; ++i) {
+				struct of_ir_register *src = ins->srcs[i];
+
+				if (!src)
+					break;
+
+				if (src->type != OF_IR_REG_R)
+					continue;
+
+				src->ver = ssa->renames[src->num];
+			}
+
+			if (dst && dst->type == OF_IR_REG_R) {
+				dst->ver = ++ssa->def_count[dst->num];
+				ssa->renames[dst->num] = dst->ver;
+			}
+		}
+		break;
+
+	default:
+		break;
+	}
+}
+
+static void
 init_nodes(struct of_ir_ssa *ssa, struct of_ir_ast_node *node)
 {
 	struct of_ir_ast_node *child;
@@ -268,12 +379,19 @@ of_ir_to_ssa(struct of_ir_shader *shader)
 	ssa->num_vars = shader->stats.num_vars;
 	ssa->vars_bitmap_size = OF_BITMAP_WORDS_FOR_BITS(ssa->num_vars)
 				* sizeof(uint32_t);
+	ssa->renames_stack = of_stack_create(ssa->num_vars
+						* sizeof(*ssa->renames), 16);
+	ssa->renames = of_stack_push(ssa->renames_stack);
+	memset(ssa->renames, 0, ssa->num_vars * sizeof(*ssa->renames));
+	ssa->def_count = of_heap_alloc(ssa->heap, ssa->num_vars
+					* sizeof(*ssa->def_count));
 
 	LIST_FOR_EACH_ENTRY(node, &shader->root_nodes, parent_list) {
 		init_nodes(ssa, node);
 		variables_defined(ssa, node);
 		dep_rep_count(ssa, node);
 		insert_phi(ssa, node);
+		make_ssa(ssa, node);
 	}
 
 	of_ir_dump_ast(shader, dump_ssa_data, ssa);
