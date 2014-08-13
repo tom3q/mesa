257cbd068c359257209857bc062507f2cad7872e
Tomasz Figa <tomasz.figa@gmail.com>
2014-08-13 20:10:34 +0200

    openfimg: compiler: Move new liveness pass to optimizer module
    Signed-off-by: Tomasz Figa <tomasz.figa@gmail.com>

--- src/gallium/drivers/openfimg/compiler/openfimg_ssa.c
+++ src/gallium/drivers/openfimg/compiler/openfimg_ssa.c
@@ -392,7 +392,7 @@ of_ir_to_ssa(struct of_ir_shader *shader)
 
 	shader->stats.num_vars = opt->last_var;
 
-	DBG("AST (post-opt/pre-optimize)");
+	DBG("AST (post-ssa/pre-optimize)");
 	of_ir_dump_ast(shader, dump_ssa_data, opt);
 
 	of_stack_destroy(opt->renames_stack);
