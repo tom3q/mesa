f70c4671f90c777b3f8abbca4b612399578eaf24
Tomasz Figa <tomasz.figa@gmail.com>
2014-08-13 22:54:09 +0200

    openfimg: compiler: Implement shader assembler
    Signed-off-by: Tomasz Figa <tomasz.figa@gmail.com>

--- src/gallium/drivers/openfimg/compiler/openfimg_ir.c
+++ src/gallium/drivers/openfimg/compiler/openfimg_ir.c
@@ -347,10 +347,8 @@ insert_instr(struct of_ir_shader *shader, struct of_ir_ast_node *node,
 		}
 	}
 
-	++shader->num_instrs;
 	list_addtail(&instr->list, list);
 	instr->node = node;
-	++node->list.num_instrs;
 }
 
 void
@@ -948,179 +946,11 @@ of_ir_dump_ast(struct of_ir_shader *shader, dump_ast_callback_t extra,
 		dump_node(shader, node, 0, extra, extra_data);
 }
 
-/*
- * Assembler.
- */
-#if 0
-static uint32_t
-dst_mask(struct of_ir_register *reg)
-{
-	uint32_t swiz = 0x0;
-	int i;
-
-	DEBUG_MSG("alu dst R%d.%s", reg->num, reg->swizzle);
-
-	if (!reg->swizzle)
-		return 0xf;
-	
-	for (i = 3; i >= 0; i--) {
-		swiz <<= 1;
-		if (reg->swizzle[i] == "xyzw"[i]) {
-			swiz |= 0x1;
-		} else if (reg->swizzle[i] != '_') {
-			ERROR_MSG("invalid dst swizzle: %s", reg->swizzle);
-			break;
-		}
-	}
-
-	return swiz;
-}
-
-static uint32_t
-src_swiz(struct of_ir_register *reg)
-{
-	uint32_t swiz = 0x00;
-	int i;
-
-	DEBUG_MSG("vector src R%d.%s", reg->num, reg->swizzle);
-
-	if (!reg->swizzle)
-		return 0xe4;
-
-	for (i = 0; i < 4; ++i) {
-		swiz >>= 2;
-		switch (reg->swizzle[i]) {
-		case 'x': swiz |= 0x0 << 6; break;
-		case 'y': swiz |= 0x1 << 6; break;
-		case 'z': swiz |= 0x2 << 6; break;
-		case 'w': swiz |= 0x3 << 6; break;
-		default:
-			ERROR_MSG("invalid vector src swizzle: %s",
-					reg->swizzle);
-		}
-	}
-
-	return swiz;
-}
-
-static void
-instr_emit(struct of_ir_shader *shader, struct of_ir_instruction *instr,
-	   uint32_t *buffer, unsigned pc)
-{
-	uint32_t *dwords = buffer + 4 * pc;
-	const struct of_ir_reg_info *reg;
-	struct of_ir_register *src0_reg;
-	struct of_ir_register *src1_reg;
-	struct of_ir_register *src2_reg;
-	struct of_ir_register *dst_reg;
-
-	memset(dwords, 0, 4 * sizeof(uint32_t));
-
-	dwords[2] |= INSTR_WORD2_OPCODE(instr->opc);
-
-	if (instr->flags & OF_IR_INSTR_NEXT_3SRC)
-		dwords[2] |= INSTR_WORD2_NEXT_3SRC;
-
-	/* Source register 1 */
-	src2_reg = instr->srcs[2];
-	if (src2_reg) {
-		reg = &shader->reg_info[src2_reg->type];
-
-		dwords[0] |= ALU_WORD0_SRC2_NUM(src2_reg->num) |
-				ALU_WORD0_SRC2_TYPE(reg->src_type) |
-				ALU_WORD0_SRC2_SWIZZLE(src_swiz(src2_reg));
-
-		if (src2_reg->flags & OF_IR_REG_NEGATE)
-			dwords[0] |= ALU_WORD0_SRC2_NEGATE;
-
-		if (src2_reg->flags & OF_IR_REG_ABS)
-			dwords[0] |= ALU_WORD0_SRC2_ABS;
-	}
-
-	/* Source register 1 */
-	src1_reg = instr->srcs[1];
-	if (src1_reg) {
-		reg = &shader->reg_info[src1_reg->type];
-
-		dwords[0] |= ALU_WORD0_SRC1_NUM(src1_reg->num);
-		dwords[1] |= ALU_WORD1_SRC1_TYPE(reg->src_type) |
-				ALU_WORD1_SRC1_SWIZZLE(src_swiz(src1_reg));
-
-		if (src1_reg->flags & OF_IR_REG_NEGATE)
-			dwords[1] |= ALU_WORD1_SRC1_NEGATE;
-
-		if (src1_reg->flags & OF_IR_REG_ABS)
-			dwords[1] |= ALU_WORD1_SRC1_ABS;
-	}
-
-	/* Source register 0 (always used) */
-	src0_reg = instr->srcs[0];
-	if (src0_reg) {
-		reg = &shader->reg_info[src0_reg->type];
-
-		dwords[1] |= INSTR_WORD1_SRC0_NUM(src0_reg->num) |
-				INSTR_WORD1_SRC0_TYPE(reg->src_type);
-
-		if (src0_reg->flags & OF_IR_REG_NEGATE)
-			dwords[1] |= INSTR_WORD1_SRC0_NEGATE;
-
-		if (src0_reg->flags & OF_IR_REG_ABS)
-			dwords[1] |= INSTR_WORD1_SRC0_ABS;
-
-		dwords[2] |= INSTR_WORD2_SRC0_SWIZZLE(src_swiz(src0_reg));
-	}
-
-	/* Destination register */
-	dst_reg = instr->dst;
-	if (dst_reg) {
-		reg = &shader->reg_info[dst_reg->type];
-
-		dwords[2] |= ALU_WORD2_DST_NUM(dst_reg->num) |
-				ALU_WORD2_DST_TYPE(reg->dst_type) |
-				ALU_WORD2_DST_MASK(dst_mask(dst_reg));
-
-		if (dst_reg->flags & OF_IR_REG_SAT)
-			dwords[2] |= ALU_WORD2_DST_SAT;
-	}
-
-	if (instr->target) {
-		int offset = instr->target->address - pc;
-		bool backwards = offset < 0;
-
-		if (backwards) {
-			dwords[2] |= CF_WORD2_JUMP_BACK;
-			offset = -offset;
-		}
-
-		dwords[2] |= CF_WORD2_JUMP_OFFS(offset);
-	}
-
-	/* TODO: Implement predicate support */
-}
-#endif
-
-static int
-of_ir_insert_cf(struct of_ir_shader *shader)
-{
-	/* TODO */
-	return 0;
-}
-
 int
 of_ir_shader_assemble(struct of_context *ctx, struct of_ir_shader *shader,
 		      struct of_shader_stateobj *so)
 {
-	uint32_t *dwords = NULL;
-	struct pipe_resource *buffer;
-	struct pipe_transfer *transfer;
 	int ret;
-
-	if (!shader->num_instrs) {
-		DBG("got empty shader");
-		pipe_resource_reference(&so->buffer, NULL);
-		so->num_instrs = 0;
-		return 0;
-	}
 
 	DBG("AST (pre-clean)");
 	of_ir_dump_ast(shader, NULL, NULL);
@@ -1148,44 +978,15 @@ of_ir_shader_assemble(struct of_context *ctx, struct of_ir_shader *shader,
 		return -1;
 	}
 
-	ret = of_ir_insert_cf(shader);
+	ret = of_ir_generate_code(ctx, shader);
 	if (ret) {
-		ERROR_MSG("failed to insert CF instructions");
+		ERROR_MSG("failed to generate code");
 		return -1;
 	}
-
-	buffer = pipe_buffer_create(ctx->base.screen,
-					PIPE_BIND_CUSTOM, PIPE_USAGE_IMMUTABLE,
-					16 * shader->num_instrs);
-	if (!buffer) {
-		ERROR_MSG("shader BO allocation failed");
-		return -1;
-	}
-
-	dwords = pipe_buffer_map(&ctx->base, buffer, PIPE_TRANSFER_WRITE,
-					&transfer);
-	if (!dwords) {
-		ERROR_MSG("failed to map shader BO");
-		goto fail;
-	}
-
-	/*
-	 * TODO: Emit instructions.
-	 */
-
-	if (transfer)
-		pipe_buffer_unmap(&ctx->base, transfer);
 
 	pipe_resource_reference(&so->buffer, NULL);
-	so->buffer = buffer;
-	so->num_instrs = shader->num_instrs;
+	so->buffer = shader->buffer;
+	so->num_instrs = shader->stats.num_instrs;
 
 	return 0;
-
-fail:
-	if (transfer)
-		pipe_buffer_unmap(&ctx->base, transfer);
-	if (buffer)
-		pipe_resource_reference(&buffer, NULL);
-	return -1;
 }
