b95e2c662452499582cd519a876eb6ca61366a6e
Tomasz Figa <tomasz.figa@gmail.com>
2014-09-14 16:56:51 +0200

    openfimg: compiler: Move few IR functions to of_shader namespace
    Signed-off-by: Tomasz Figa <tomasz.figa@gmail.com>

--- src/gallium/drivers/openfimg/compiler/openfimg_compiler.h
+++ src/gallium/drivers/openfimg/compiler/openfimg_compiler.h
@@ -27,6 +27,12 @@
 #include "openfimg_program.h"
 #include "openfimg_util.h"
 
-int of_compile_shader(struct of_shader_stateobj *so);
+int of_shader_compile(struct of_shader_stateobj *so);
+int of_shader_assemble(struct of_context *ctx, struct of_ir_shader *shader,
+			  struct of_shader_stateobj *so);
+void of_shader_destroy(struct of_ir_shader *shader);
+
+int of_shader_disassemble(struct of_context *ctx, struct pipe_resource *buffer,
+			     unsigned sizedwords, enum of_shader_type type);
 
 #endif /* OF_COMPILER_H_ */
